77eed2463df2006addc6101d59cb9e635f6c8262
Denis FARKAS <denis.farkas@laplateforme.io>
2024-03-16 09:18:47 +0100

    two times

--- src/databases/init.database.js
+++ src/databases/init.database.js
@@ -3,10 +3,10 @@ import mysql from "mysql";
 // Création d'un gestionnaire (ou pool) de connexions à la base de données MySQL
 const pool = mysql.createPool({
   connectionLimit: 10000,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // Fonction pour exécuter une requête SQL
