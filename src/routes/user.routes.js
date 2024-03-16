6320231b108f33a9ad3482322e25f34a60c0e921
Denis FARKAS <denis.farkas@laplateforme.io>
2024-03-16 09:28:30 +0100

    three time

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,21 +4,22 @@ import { UserController } from "../controllers/user.controller.js";
 
 // Fonction pour initialiser les routes liées aux utilisateurs dans l'application Express
 const initUserRoutes = (app) => {
-    // Création d'un routeur Express dédié aux routes des utilisateurs
-    const router = express.Router();
+  // Création d'un routeur Express dédié aux routes des utilisateurs
+  const router = express.Router();
 
-    // Définition des routes avec les méthodes associées du contrôleur
-    router.post("/signUp", UserController.signUp);
-    router.post("/signIn", UserController.signIn);
-    router.get("/read", UserController.read);
-    router.get("/readOneUser", UserController.readOneUser);
-    router.post("/sign-up-workshop", UserController.signUpWorkshop);
-    router.post("/isRegistered", UserController.isRegistered);
-    router.get("/registeredWorkshop", UserController.registeredWorkshop);
-    router.put("/updateUser", jwt, UserController.updateUser);
+  // Définition des routes avec les méthodes associées du contrôleur
+  router.post("/signUp", UserController.signUp);
+  router.post("/signIn", UserController.signIn);
+  router.get("/read", UserController.read);
+  router.get("/readOneUser", UserController.readOneUser);
+  router.post("/sign-up-workshop", UserController.signUpWorkshop);
+  router.post("/isRegistered", UserController.isRegistered);
+  router.get("/registeredWorkshop", UserController.registeredWorkshop);
+  router.put("/updateUser", jwt, UserController.updateUser);
+  router.get("/test", UserController.test);
 
-    // Utilisation du routeur dans l'application avec le préfixe "/users"
-    app.use("/users", router);
+  // Utilisation du routeur dans l'application avec le préfixe "/users"
+  app.use("/users", router);
 };
 
 export default initUserRoutes;
